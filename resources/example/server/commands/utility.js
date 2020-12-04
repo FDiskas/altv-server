43b48efbf4e84bdf031c34852485382ee8f572fb
Vytenis Kuciauskas <projektas@gmail.com>
2020-12-04 20:02:28 +0200

    Add /help command

--- resources/example/server/commands/utility.js
+++ resources/example/server/commands/utility.js
@@ -9,3 +9,15 @@ registerCmd('coords', '/coords | Returns current coordinates to chat and console
 registerCmd('players', '/players | Returns current player count.', player => {
     player.send(`Player Count: ${alt.Player.all.length}`);
 });
+
+registerCmd('help', '/help | Returns available commands to console.', player => {
+    const coords = player.pos;
+    player.send(`
+    /ped <name> | Spawn character by name.
+    /ve <name> | Summons a vehicle in front of a player.
+    /wep <name> | Summon a weapon by name.
+    /re | Respawn the player at spawn.
+    /exit | Exit (kick) from the game.
+    `);
+    console.log(coords);
+});
